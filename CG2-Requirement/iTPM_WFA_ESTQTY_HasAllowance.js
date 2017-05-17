89224d36163674f3a48f696a86389db395745f3d
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-05-17 16:56:29 +0530

    taj:-removed the logs from the estqty has allowance script

--- CG2-Requirement/iTPM_WFA_ESTQTY_HasAllowance.js
+++ CG2-Requirement/iTPM_WFA_ESTQTY_HasAllowance.js
@@ -20,8 +20,8 @@ function(search,record,runtime) {
 			var estVolumeRec = scriptContext.newRecord,
 			scriptObj = runtime.getCurrentScript();
 			
-			var promoDealId = scriptObj.getParameter({name:'custscript_itpm_estqty_hasallowance_item'}),
-			estVolumeItemId = scriptObj.getParameter({name:'custscript_itpm_estqty_hasallownce_promo'});
+			var promoDealId = scriptObj.getParameter({name:'custscript_itpm_estqty_hasallownce_promo'}),
+			estVolumeItemId = scriptObj.getParameter({name:'custscript_itpm_estqty_hasallowance_item'});
 			
 			var promoDealAllowanceSearch = search.create({
 				type:'customrecord_itpm_promotiondeal',
@@ -31,8 +31,8 @@ function(search,record,runtime) {
 					['custrecord_itpm_all_promotiondeal.custrecord_itpm_all_item','is',estVolumeItemId],'and',
 					['custrecord_itpm_all_promotiondeal.isinactive','is',false]]
 			});
-			
-			return (promoDealAllowanceSearch.run().getRange(0,10).length == 0)?'T':'F';
+
+			return (promoDealAllowanceSearch.run().getRange(0,10).length > 0)?'T':'F';
 			
 		}catch(e){
 			log.debug('exception in allowance duplicated detection',e)
