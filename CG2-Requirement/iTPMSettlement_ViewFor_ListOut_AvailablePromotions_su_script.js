f94bc1b494e59ed3e732a9900fc88648157e138f
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-06-13 21:38:49 +0530

    taj:-changed the git ignore file

--- CG2-Requirement/iTPMSettlement_ViewFor_ListOut_AvailablePromotions_su_script.js
+++ CG2-Requirement/iTPMSettlement_ViewFor_ListOut_AvailablePromotions_su_script.js
@@ -118,7 +118,7 @@ function(serverWidget,redirect,search,record) {
 					    [
 					    	[['custrecord_itpm_p_type.custrecord_itpm_pt_settlewhenpromoactive','is','T'],'and',
 					    	['custrecord_itpm_p_condition','is',2]],'or', //active if promotion type allow for settlemen in active
-					    	['custrecord_itpm_p_condition','is', 3] //completed
+					    	['custrecord_itpm_p_condition','is', 3]   //completed
 					    ],'and',
 //					    ['custrecord_itpm_p_condition','is', 3],'and', //completed
 						['custrecord_itpm_p_customer','is', deductionRec.custbody_itpm_ddn_customer[0].value],'and',
