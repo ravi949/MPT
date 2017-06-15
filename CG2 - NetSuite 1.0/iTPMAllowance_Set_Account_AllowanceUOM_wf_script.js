8a8117e89cd3f3e8a844b78a9e9f8035f7765cc1
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-06-15 15:14:37 +0530

    taj:-changed the allowance calulations

--- CG2 - NetSuite 1.0/iTPMAllowance_Set_Account_AllowanceUOM_wf_script.js
+++ CG2 - NetSuite 1.0/iTPMAllowance_Set_Account_AllowanceUOM_wf_script.js
@@ -14,7 +14,7 @@ function setAccountAndAllowanceUOM() {
 		var accountValue = nlapiGetFieldValue('custrecord_itpm_all_account'), //changed from custpage_custom_account
 		allowanceUOMValue = nlapiGetFieldValue('custrecord_itpm_all_uom'), //changed from dyn field
 		promoDealId = nlapiGetFieldValue('custrecord_itpm_all_promotiondeal'),
-		itemBasePrice = nlapiGetFieldValue('custrecord_itpm_all_itembaseprice');
+		impactPrice = nlapiGetFieldValue('custrecord_itpm_all_impactprice');
 		if(accountValue && accountValue !=''){
 			nlapiSetFieldValue('custrecord_itpm_all_account',accountValue , true, true);
 		}
@@ -69,24 +69,26 @@ function setAccountAndAllowanceUOM() {
 			conversionFactor = allowanceUOMRate/conversionRate;
 			//Story S-01197:-Prices on the Allowances sub-tab should be shown in the UOM that the user selected when creating the allowance
 			var saleUnitList = unitsList.filter(function(e){return e.id == itemResult[0].getValue('saleunit')});
-			nlapiSetFieldValue('custrecord_itpm_all_uomprice',(itemBasePrice)*(allowanceUOMRate/saleUnitList[0].rate))
+			nlapiSetFieldValue('custrecord_itpm_all_uomprice',(impactPrice)*(allowanceUOMRate/saleUnitList[0].rate))
 		}
-
-//		var itemPrice = nlapiGetFieldValue('custrecord_itpm_all_impactprice'),
-		var itemPrice = nlapiGetFieldValue('custrecord_itpm_all_uomprice'),
+		
+//		var allUnitPrice = nlapiGetFieldValue('custrecord_itpm_all_impactprice'),
+		var allUnitPrice = nlapiGetFieldValue('custrecord_itpm_all_uomprice'),
 		allowanceType = nlapiGetFieldValue('custrecord_itpm_all_type'),
 		redemptionFactor = parseFloat(nlapiGetFieldValue('custrecord_itpm_all_redemptionfactor'))/100;
 
 		switch(allowanceType){
 		case '1': //Rate per UOM calculate %Discount price
 			var allowanceRateValue = parseFloat(nlapiGetFieldValue('custrecord_itpm_all_allowancerate'));
-			var percentBasePrice = (itemPrice !='' && itemPrice != 0) ? (allowanceRateValue/(conversionFactor*itemPrice))*100:0;
+//			var percentBasePrice = (allUnitPrice !='' && allUnitPrice != 0) ? (allowanceRateValue/(conversionFactor*allUnitPrice))*100:0;
+			var percentBasePrice = (allUnitPrice !='' && allUnitPrice != 0) ? (allowanceRateValue/allUnitPrice)*100:0;
 			nlapiSetFieldValue('custrecord_itpm_all_percentperuom',parseFloat(percentBasePrice));
 			nlapiSetFieldValue('custrecord_itpm_all_allowancepercent',parseFloat(percentBasePrice));
 			break;
 		case '2': //%Discount calculate rate UOM
 			var percentBasePrice = parseFloat(nlapiGetFieldValue('custrecord_itpm_all_allowancepercent'))/100;
-			var ratePerUOM = percentBasePrice * conversionFactor * itemPrice * redemptionFactor;
+//			var ratePerUOM = percentBasePrice * conversionFactor * allUnitPrice * redemptionFactor;
+			var ratePerUOM = percentBasePrice * allUnitPrice * redemptionFactor;
 			nlapiSetFieldValue('custrecord_itpm_all_rateperuom',ratePerUOM);
 			nlapiSetFieldValue('custrecord_itpm_all_allowancerate',ratePerUOM);
 			break;
