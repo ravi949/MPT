61c74ab27c3d5e2f01a1433803b2d4f71578f493
Nagaraju Mantrabuddi <Nagaraju@amzurtech.com>
2017-06-19 21:17:54 +0530

    Nagaraju:- adding the lines for creting a JE

--- CG2-Requirement/iTPM_WFA_DDN_To_ReInvoice.js
+++ CG2-Requirement/iTPM_WFA_DDN_To_ReInvoice.js
@@ -20,14 +20,93 @@ function(record, redirect, runtime) {
      * @Since 2016.1
      */
     function onAction(scriptContext) {
-    	var ddn = scriptContext.newRecord;
-    	log.debug('ddn',ddn.id);
+    	try{
+    	var DeductionRec = scriptContext.newRecord;
+    	/*log.debug('ddn',ddn.id);
     	log.debug('ddn',ddn.getValue({fieldId:'custbody_itpm_ddn_disputed' }));
     	redirect.toTaskLink({
     		id:'EDIT_TRAN_CUSTINVC',
     		parameters:{ddn:ddn.id}
-    	});
-//    	return ddn.getValue({fieldId:'custbody_itpm_ddn_disputed' })
+    	});*/
+    	
+    	var JERec = record.create({
+			type:record.Type.JOURNAL_ENTRY
+		})
+		
+		JERec.setValue({
+			fieldId:'subsidiary',
+			value:DeductionRec.getValue('subsidiary')
+		}).setValue({
+			fieldId:'currency',
+			value:DeductionRec.getValue('currency')
+		}).setValue({
+			fieldId:'custbody_itpm_set_deduction',
+			value:DeductionRec.id
+		}).setValue({
+			fieldId:'memo',
+			value:DeductionRec.getValue('memo')
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'account',
+			value:DeductionRec.getSublistValue({sublistId: 'line',fieldId: 'account',line: 0	}),
+			line:0
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'debit',
+			value:DeductionRec.getValue('custbody_itpm_ddn_openbal'),
+			line:0
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'entity',
+			value:DeductionRec.getValue('custbody_itpm_ddn_customer'),
+			line:0
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'memo',
+			value:DeductionRec.getValue('memo'),
+			line:0
+		})
+		
+		JERec.setSublistValue({
+			sublistId:'line',
+			fieldId:'account',
+			value:DeductionRec.getSublistValue({sublistId: 'line',fieldId: 'account',line: 1}),
+			line:1
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'credit',
+			value:DeductionRec.getValue('custbody_itpm_ddn_openbal'),
+			line:1
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'entity',
+			value:DeductionRec.getValue('custbody_itpm_ddn_customer'),
+			line:1
+		}).setSublistValue({
+			sublistId:'line',
+			fieldId:'memo',
+			value:DeductionRec.getValue('memo'),
+			line:1
+		})
+		
+		JERecId = JERec.save({enableSourcing:false,ignoreMandatoryFields:true});
+		log.debug('je record',JERecId);
+		//changing the status of the deduction record to resolved
+		if(JERecId){
+			record.load({
+				type:'customtransaction_itpm_deduction',
+				id:DeductionRec.id
+			}).setValue({
+				fieldId:'custbody_itpm_ddn_openbal',
+				value:0
+			}).save({enableSourcing:false,ignoreMandatoryFields:true});
+		}
+		
+    
+    	}catch(e){
+    		log.error(e.name,e.message);
+    	}
+    
     }
 
     return {
