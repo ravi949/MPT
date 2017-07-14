00cd0c92abb106b83cde4583d4915adb9361287c
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-07-14 17:28:52 +0530

    taj:- combine the sales and shipment url set with promotion processing UE

--- CG2-Requirement/iTPM_UE_Promotion_Processing.js
+++ CG2-Requirement/iTPM_UE_Promotion_Processing.js
@@ -99,26 +99,86 @@ function(serverWidget,record,runtime,url) {
      * @param {string} scriptContext.type - Trigger type
      * @Since 2015.2
      */
-    function beforeSubmit(scriptContext) {
-    }
-
-    /**
-     * Function definition to be triggered before record is loaded.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.newRecord - New record
-     * @param {Record} scriptContext.oldRecord - Old record
-     * @param {string} scriptContext.type - Trigger type
-     * @Since 2015.2
-     */
     function afterSubmit(scriptContext) {
-    	
+    	try{
+			if(scriptContext.newRecord.getValue('custrecord_itpm_p_impact') == '13'){
+				//Actual Sales Suitelet URL
+				var actualSalesURL = url.resolveScript({
+					scriptId: 'customscript_itpm_promo_actualsales',
+					deploymentId: 'customdeploy_itpm_promo_actualsales',
+					returnExternalUrl: false,
+					params: {
+						'pid':scriptContext.newRecord.id,
+						'yr':0,
+						'st':0
+					}
+				});
+
+				//Actual Sales Previous Year Suitelet URL
+				var actualSalesURLPreviousYear = url.resolveScript({
+					scriptId: 'customscript_itpm_promo_actualsales',
+					deploymentId: 'customdeploy_itpm_promo_actualsales',
+					returnExternalUrl: false,
+					params: {
+						'pid':scriptContext.newRecord.id,
+						'yr':1,
+						'st':0
+					}
+				});
+				
+				//Actual Shippments Suitelet URL
+				var actualShippmentsURL = url.resolveScript({
+					scriptId: 'customscript_itpm_promo_actualshippments',
+					deploymentId: 'customdeploy_itpm_promo_actualshippments',
+					returnExternalUrl: false,
+					params: {
+						'pid':scriptContext.newRecord.id,
+						'yr':0,
+						'st':0
+					}
+				});
+				
+				//Actual Shippments Previous Year Suitelet URL
+				var actualShippmentsURLPreviousYear = url.resolveScript({
+					scriptId: 'customscript_itpm_promo_actualshippments',
+					deploymentId: 'customdeploy_itpm_promo_actualshippments',
+					returnExternalUrl: false,
+					params: {
+						'pid':scriptContext.newRecord.id,
+						'yr':1,
+						'st':0
+					}
+				});
+				//adding the Actual and Shipments URLs.
+				record.load({
+					type:'customrecord_itpm_promotiondeal',
+					id:scriptContext.newRecord.id,
+					isDynamic:true
+				}).setValue({
+					fieldId:'custrecord_itpm_p_actualsales', //Actual Sales
+					value:actualSalesURL
+				}).setValue({
+					fieldId:'custrecord_itpm_p_actualshippments', //Actual Shippments
+					value:actualShippmentsURL
+				}).setValue({
+					fieldId:'custrecord_itpm_p_actualsalespreviousyr', //Actual Sales Previous Year
+					value:actualSalesURLPreviousYear
+				}).setValue({
+					fieldId:'custrecord_itpm_p_actualshippreviousyear', //Actual Shippments Previous Year
+					value:actualShippmentsURLPreviousYear
+				}).save({
+					enableSourcing:false,
+					ignoreMandatoryFields:true
+				});
+			}
+		}catch(e){
+			log.error(e.name,'record type = iTPM promotion, record id = '+scriptContext.newRecord.id+', message = '+e.message);
+		}
     }
 
     return {
         beforeLoad: beforeLoad,
-/*        beforeSubmit: beforeSubmit,
-        afterSubmit: afterSubmit*/
+        afterSubmit: afterSubmit
     };
     
 });
