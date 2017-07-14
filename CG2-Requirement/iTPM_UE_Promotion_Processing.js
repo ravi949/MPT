ee2b8a4611cb39d24f49ae0f0908439ee66466e6
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-07-14 16:17:17 +0530

    taj:-merge the subtab and new settlement UE script

--- CG2-Requirement/iTPM_UE_Promotion_Processing.js
+++ CG2-Requirement/iTPM_UE_Promotion_Processing.js
@@ -3,12 +3,14 @@
  * @NScriptType UserEventScript
  * @NModuleScope TargetAccount
  */
-define(['N/ui/serverWidget','N/record'],
+define(['N/ui/serverWidget','N/record','N/runtime','N/url'],
 /**
  * @param {serverWidget} serverWidget
  * @param {record} record
+ * @param {runtime} runtime
+ * @param {url} url
  */
-function(serverWidget,record) {
+function(serverWidget,record,runtime,url) {
    
     /**
      * Function definition to be triggered before record is loaded.
@@ -23,26 +25,53 @@ function(serverWidget,record) {
     	try{
     		var promoRec = scriptContext.newRecord;
     		var promoForm = scriptContext.form;
-    		if(scriptContext.type == 'view'){
-    			var status = promoRec.getValue('custrecord_itpm_p_status');
-    			var condition = promoRec.getValue('custrecord_itpm_p_condition');
-    			
-    			//ALLOW SETTLEMENTS WHEN PROMOTION IS ACTIVE?
-    			var allowForSettlement = record.load({
-    				type:'customrecord_itpm_promotiontype',
-    				id:promoRec.getValue('custrecord_itpm_p_type')
-    			}).getValue('custrecord_itpm_pt_settlewhenpromoactive');
-    			
-    			var showSettlementButton = ((status == 3 && condition == 3) || (allowForSettlement && (status == 3 && condition == 2)));
-    			
-    			if(showSettlementButton){
-    				promoForm.addButton({
-    					id:'custpage_newsettlementbtn',
-    					label:'New Settlement',
-    					functionName:'newSettlement('+promoRec.id+')'
+    		if(runtime.executionContext == runtime.ContextType.USER_INTERFACE){
+ 
+    			//this block for adding the New Settement button to Promotion record.
+    			if(scriptContext.type == 'view'){
+        			var status = promoRec.getValue('custrecord_itpm_p_status');
+        			var condition = promoRec.getValue('custrecord_itpm_p_condition');
+        			
+        			//ALLOW SETTLEMENTS WHEN PROMOTION IS ACTIVE?
+        			var allowForSettlement = record.load({
+        				type:'customrecord_itpm_promotiontype',
+        				id:promoRec.getValue('custrecord_itpm_p_type')
+        			}).getValue('custrecord_itpm_pt_settlewhenpromoactive');
+        			
+        			var showSettlementButton = ((status == 3 && condition == 3) || (allowForSettlement && (status == 3 && condition == 2)));
+        			
+        			if(showSettlementButton){
+        				promoForm.addButton({
+        					id:'custpage_newsettlementbtn',
+        					label:'New Settlement',
+        					functionName:'newSettlement('+promoRec.id+')'
+        				});
+        				promoForm.clientScriptModulePath = './iTPM_Attach_Promotion_ClientMethods.js';
+        			}
+        		}
+        		
+    			//this block for showing the overlapping promotions in promotion record subtab
+        		if(scriptContext.type == 'view' || scriptContext.type == 'edit'){
+        			var customerId = promoRec.getValue('custrecord_itpm_p_customer');
+        			var startDate = promoRec.getText('custrecord_itpm_p_shipstart');
+        			var endDate = promoRec.getText('custrecord_itpm_p_shipend');
+        			//setting the overlap promotion suitelet url as value to inline html field.
+        			var overlapSuiteletURL = url.resolveScript({
+    					scriptId: 'customscript_itpm_promo_overlaplist',
+    					deploymentId: 'customdeploy_itpm_promo_overlaplist',
+    					returnExternalUrl: false,
+    					params: {
+    						cid:customerId,
+    						pdid:promoRec.id,
+    						start:startDate,
+    						end:endDate
+    					}
     				});
-    				promoForm.clientScriptModulePath = './iTPM_Attach_Promotion_ClientMethods.js';
-    			}
+        			promoRec.setValue({
+    					fieldId:'custrecord_itpm_p_overlappingpromotions',
+    					value:'<iframe id="boxnet_widget_frame" src="'+overlapSuiteletURL+'" align="center" style="width: 100%; height:600px; margin:0; border:0; padding:0" frameborder="0"></iframe>'
+    				});
+        		}
     		}
     	}catch(e){
     		log.error(e.name,'record id = '+scriptContext.newRecord.id+', function name = beforeload, message = '+e.message);
