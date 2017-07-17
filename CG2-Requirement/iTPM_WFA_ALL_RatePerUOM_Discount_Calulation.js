e73da809a63cfcaf1d4c8daecf022d8c5818aa86
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-07-17 18:01:45 +0530

    taj:-updated the allowance rate calculation script

--- CG2-Requirement/iTPM_WFA_ALL_RatePerUOM_Discount_Calulation.js
+++ CG2-Requirement/iTPM_WFA_ALL_RatePerUOM_Discount_Calulation.js
@@ -18,57 +18,23 @@ function(record) {
      */
     function onAction(scriptContext) {
     	try{
-    		var allRec = scriptContext.newRecord,
-    		itemId = allRec.getValue('custrecord_itpm_all_item'),
-    		allUOM = allRec.getValue('custrecord_itpm_all_uom'), //changed from dyn field
-    		promoId = allRec.getValue('custrecord_itpm_all_promotiondeal'),
-    		allUnitPrice =  parseFloat(allRec.getValue('custrecord_itpm_all_uomprice')),
-    		allType = allRec.getValue('custrecord_itpm_all_type'),
-    		redemptionFactor = parseFloat(allRec.getValue('custrecord_itpm_all_redemptionfactor'))/100;
-    		
-    		//for this you need to load the iTPM_Module.js file
-//    		var unitsList = iTPM_Module.getItemUnits(itemId).unitArray;
+    		var allRec = scriptContext.newRecord;
+    		var itemId = allRec.getValue('custrecord_itpm_all_item');
+    		var allUOM = allRec.getValue('custrecord_itpm_all_uom'); //changed from dyn field
+    		var promoId = allRec.getValue('custrecord_itpm_all_promotiondeal');
+    		var allUnitPrice =  parseFloat(allRec.getValue('custrecord_itpm_all_uomprice'));
+    		var allType = allRec.getValue('custrecord_itpm_all_type');
+    		var redemptionFactor = parseFloat(allRec.getValue('custrecord_itpm_all_redemptionfactor'))/100;
 
-    		//allowance uom price calculation based on item converstion rate 
-//    		if(allowanceUOMList.length>0){
-//    			var allowanceUOMRate = allowanceUOMList[0].rate;
-//    			conversionFactor = allowanceUOMRate/conversionRate;
-//    			//Story S-01197:-Prices on the Allowances sub-tab should be shown in the UOM that the user selected when creating the allowance
-//    			var saleUnitList = unitsList.filter(function(e){return e.id == itemResult[0].getValue('saleunit')});
-//    			nlapiSetFieldValue('custrecord_itpm_all_uomprice',(impactPrice)*(allowanceUOMRate/saleUnitList[0].rate))
-//    		}
-    		
-    		
-//    		var promoImpact = record.load({
-//    			type:'customrecord_itpm_promotiondeal',
-//    			id:promoId
-//    		}).getValue('custrecord_itpm_p_impact');
-//    		
-//    		var itemSearch = search.create({
-//    			type:search.Type.ITEM,
-//    			columns:['unitstype','saleunit','purchaseunit'],
-//    			filters:[['internalid','is',itemId],'and',['isinactive','is',false]]
-//    		}).run().getRange(0,1),
-//    		itemRecItemType = itemSearch[0].getValue('unitstype'),
-//    		itemSaleOrPurchaseUnitType;
-//    		
-//    		if(promoImpact == '13'){
-//    			itemSaleOrPurchaseUnitType = itemSearch[0].getValue('saleunit');
-//    		}else{
-//    			itemSaleOrPurchaseUnitType = itemSearch[0].getValue('purchaseunit');
-//    		}
-    		
     		switch(allType){
     		case '1': //Rate per UOM calculate %Discount price
     			var allowanceRateValue = parseFloat(allRec.getValue('custrecord_itpm_all_allowancerate'));
-//    			var percentBasePrice = (allUnitPrice !='' && allUnitPrice != 0) ? (allowanceRateValue/(conversionFactor*allUnitPrice))*100:0;
     			var percentBasePrice = (allUnitPrice !='' && allUnitPrice != 0) ? (allowanceRateValue/allUnitPrice)*100:0;
     			allRec.setValue('custrecord_itpm_all_percentperuom',parseFloat(percentBasePrice));
     			allRec.setValue('custrecord_itpm_all_allowancepercent',parseFloat(percentBasePrice));
     			break;
     		case '2': //%Discount calculate rate UOM
     			var percentBasePrice = parseFloat(allRec.getValue('custrecord_itpm_all_allowancepercent'))/100;
-//    			var ratePerUOM = percentBasePrice * conversionFactor * allUnitPrice * redemptionFactor;
     			var ratePerUOM = percentBasePrice * allUnitPrice * redemptionFactor;
     			allRec.setValue('custrecord_itpm_all_rateperuom',ratePerUOM);
     			allRec.setValue('custrecord_itpm_all_allowancerate',ratePerUOM);
