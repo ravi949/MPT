a50fa048061dab72ca43dcc9c351261e4b0fbfef
Nagaraju Mantrabuddi <Nagaraju@amzurtech.com>
2017-09-11 22:08:22 +0530

    Nagaraju:- adding the Net Bill process for goverance usage

--- iTPM_UE_NBOI_Processing.js
+++ iTPM_UE_NBOI_Processing.js
@@ -19,51 +19,29 @@ define(['N/redirect', 'N/search', 'N/runtime', 'N/record'],
 	 */
 	function afterSubmit(scriptContext) {
 		try{
-			var transRec = record.load({
+			
+			var transRecord =record.load({
 				type: scriptContext.newRecord.type, 
 				id: scriptContext.newRecord.id,
 				isDynamic: true
 			});
-			if(scriptContext.type == 'create' && transRec.getValue('custbody_itpm_applydiscounts')){				
-				var customer = transRec.getValue('entity');
-				var date = transRec.getText('trandate'); 
+//			if(scriptContext.type == 'create' && transRec.getValue('custbody_itpm_applydiscounts')){
+			if(scriptContext.type == 'create'){
 
-				var estGov = 0;
 				//fetching Preferance data
 				var prefDatesType = getPrefDiscountDateValue();
-				estGov = estGov + 12;
-				//load transaction
-				estGov = estGov + 10;
-				var itemCount = transRec.getLineCount({
-					sublistId: 'item'
-				});
-				//for loop start
-				for(var i=0; i<itemCount; i++){
-					var lineItem = transRec.getSublistValue({
-						sublistId : 'item',
-						fieldId   : 'item',
-						line      : i
-					});
-					estGov = estGov + 6;
-					var itemResults = getAllowanceItems(prefDatesType, lineItem, customer, date);
+				log.debug('prefDatesType',prefDatesType);
+				var nbEstGov = getEstGovernance('2', transRecord.id, transRecord.type, prefDatesType);
+				var oiEstGov = getEstGovernance('3', transRecord.id, transRecord.type, prefDatesType);
 
-					itemResults.each(function(result){
-						estGov = estGov + 10; //record validation
-						estGov = estGov + 12; //to create DL & DLL custom records (approx)
-						return true;
-					});
-					//for loop end
-					estGov = estGov + 10;
-				}
-				//transaction record commit after discount'
-				estGov = estGov + 20;
-				log.debug('Usage : Final', estGov);
+				log.debug('Usage : nbEstGov', nbEstGov);
+				log.debug('Usage : oiEstGov', oiEstGov);
 				log.debug('UE FINAL: ', runtime.getCurrentScript().getRemainingUsage());
-				if(estGov < 900){
+				if(nbEstGov < 900 && oiEstGov < 900){
 					redirect.toSuitelet({
 						scriptId: 'customscript_itpm_nb_processing' ,
 						deploymentId: 'customdeploy_itpm_nb_processing',
-						parameters: {'id':transRec.id,'type':transRec.type} 
+						parameters: {'id':transRecord.id,'type':transRecord.type} 
 					});
 				}
 			}
@@ -72,11 +50,54 @@ define(['N/redirect', 'N/search', 'N/runtime', 'N/record'],
 		}
 
 	}
-	function getAllowanceItems(prefDatesType, item, customer, trandate){
+	function getEstGovernance(allowanceType, transRecId, transRecType, prefDatesType){
+		
+		var transRec = record.load({
+			type: transRecType, 
+			id: transRecId,
+			isDynamic: true
+		});
+		
+		var customer = transRec.getValue('entity');
+		var date = transRec.getText('trandate'); 
+		var estGov = 0;
+		estGov = estGov + 12;//For Preferance data
+		if(allowanceType == '2'){
+			estGov = estGov + 10;//Transaction load(NB)
+		}
+		else{
+			estGov = estGov + 20;//Transaction load(OI)
+		}
+		var itemCount = transRec.getLineCount({
+			sublistId: 'item'
+		});
+		//for loop start
+		for(var i=0; i<itemCount; i++){
+			var lineItem = transRec.getSublistValue({
+				sublistId : 'item',
+				fieldId   : 'item',
+				line      : i
+			});
+			estGov = estGov + 6;
+			var itemResults = getAllowanceItems(prefDatesType, lineItem, customer, date, allowanceType);
+
+			itemResults.each(function(result){
+				estGov = estGov + 10; //record validation
+				estGov = estGov + 12; //to create DL & DLL custom records (approx)
+				return true;
+			});
+			//for loop end
+			estGov = estGov + 10;
+		}
+		//transaction record commit after discount'
+		estGov = estGov + 20;
+		return estGov;
+	}
+	function getAllowanceItems(prefDatesType, item, customer, trandate, allowanceType){
 		var tranFilters = [
 		                   ['custrecord_itpm_all_promotiondeal.custrecord_itpm_p_status','anyof','3'], //here 3 means status is Approved
 		                   'AND', 
-		                   ['custrecord_itpm_all_mop','anyof','3'], //here  means Method of Payment is  Off Invoice
+		                   ['custrecord_itpm_all_mop','anyof',allowanceType], //here  means Method of Payment is  Net Bill/Off Invoice
 		                   'AND', 
 		                   ['custrecord_itpm_all_item','anyof',item], //item from transaction line
 		                   'AND', 
