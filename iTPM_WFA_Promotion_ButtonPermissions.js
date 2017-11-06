8f37255b404681ef9a13f17874b656cfc922c115
Nagaraju Mantrabuddi <Nagaraju@amzurtech.com>
2017-11-06 21:16:07 +0530

    nag:- change the condition from if else to terninary operator

--- iTPM_WFA_Promotion_ButtonPermissions.js
+++ iTPM_WFA_Promotion_ButtonPermissions.js
@@ -21,12 +21,8 @@ function(runtime) {
 		try{
 			var userObj = runtime.getCurrentUser();
 			var userPermission = userObj.getPermission('LIST_CUSTRECORDENTRY'+scriptContext.newRecord.getValue('rectype'));
-			log.debug('userObj ',userObj );
-			if(userPermission == runtime.Permission.FULL || userPermission == runtime.Permission.EDIT || userPermission == runtime.Permission.CREATE){
-				return 'T';
-			}else{
-				return 'F';
-			}    	
+			log.debug('userPermission ',userPermission );
+			return (userPermission == runtime.Permission.FULL || userPermission == runtime.Permission.EDIT || userPermission == runtime.Permission.CREATE)?'T':'F';				
 		}catch(e){
 			log.error(e.name,e.message);
 			return 'F';
