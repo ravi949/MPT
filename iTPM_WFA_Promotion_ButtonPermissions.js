3674a03d407e06d45506180250af790e875380f7
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-11-14 14:11:22 +0530

    taj:-deleted the WFA script on reopen and replaced with existing script

--- iTPM_WFA_Promotion_ButtonPermissions.js
+++ iTPM_WFA_Promotion_ButtonPermissions.js
@@ -3,11 +3,13 @@
  * @NScriptType workflowactionscript
  * Workflow action script to check user permission of record and returns the true or false based on the record permission.
  */
-define(['N/runtime'],
+define(['N/runtime',
+		'N/record'
+	   ],
 /**
  * @param {runtime} runtime
  */
-function(runtime) {
+function(runtime,record) {
    
     /**
      * Definition of the Suitelet script trigger point.
@@ -21,12 +23,19 @@ function(runtime) {
 		try{
 			var userObj = runtime.getCurrentUser();
 			var scriptObj = runtime.getCurrentScript();
-			var userPermission = userObj.getPermission('LIST_CUSTRECORDENTRY'+scriptContext.newRecord.getValue('rectype'));
-			log.debug('userPermission ',userPermission );
-			if(scriptObj.getParameter('custscript_itpm_perm_createoredit')){
-				return (userPermission == runtime.Permission.EDIT || userPermission == runtime.Permission.CREATE)?'T':'F';
+			var permissionOnPromo = userObj.getPermission('LIST_CUSTRECORDENTRY'+scriptContext.newRecord.getValue('rectype'));
+
+			if(scriptObj.getParameter('custscript_itpm_perm_pt_promo_hasedit')){
+				var PromotypeRecord = record.load({
+	    		    type: 'customrecord_itpm_promotiontype', 
+	    		    id: scriptContext.newRecord.getValue('custrecord_itpm_p_type')    		 
+	    		});
+	    		var permissionOnPromoType = userObj.getPermission('LIST_CUSTRECORDENTRY'+PromotypeRecord.getValue('rectype'));
+	    		return ((permissionOnPromo == runtime.Permission.EDIT && permissionOnPromoType == runtime.Permission.EDIT) || (permissionOnPromoType == runtime.Permission.FULL && permissionOnPromo == runtime.Permission.FULL))?'T':'F';
+			}else if(scriptObj.getParameter('custscript_itpm_perm_createoredit')){
+				return (permissionOnPromo == runtime.Permission.EDIT || permissionOnPromo == runtime.Permission.CREATE)?'T':'F';
 			}else if(scriptObj.getParameter('custscript_itpm_perm_full')){
-				return (userPermission == runtime.Permission.FULL)?'T':'F';
+				return (permissionOnPromo == runtime.Permission.FULL)?'T':'F';
 			}
 		}catch(e){
 			log.error(e.name,e.message);
