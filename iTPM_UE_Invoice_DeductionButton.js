fc9abbefa6d9102de52efaa612259f3a6189c211
Ravishankar Bandaru <ravishankar@amzurtech.com>
2017-12-06 18:46:22 +0530

    Ravi:- Added the Deduction permission condition On open invoices

--- iTPM_UE_Invoice_DeductionButton.js
+++ iTPM_UE_Invoice_DeductionButton.js
@@ -6,53 +6,59 @@
  */
 define(['N/search',
 		'N/ui/serverWidget',
-		'N/runtime'
+		'N/runtime',
+		'./iTPM_Module.js'
 		],
-/**
- * @param {search} search
- * @param {serverWidget} serverWidget
- */
-function(search, serverWidget, runtime) {
-   
-    /**
-     * Function definition to be triggered before record is loaded.
-     *
-     * @param {Object} scriptContext
-     * @param {Record} scriptContext.newRecord - New record
-     * @param {string} scriptContext.type - Trigger type
-     * @param {Form} scriptContext.form - Current form
-     * @Since 2015.2
-     */
-    function beforeLoad(scriptContext) {
-    	try{
-    		if(runtime.executionContext == runtime.ContextType.USER_INTERFACE && scriptContext.type == 'view'){
-    			//invoice status not equal to PAID IN FULL
-    			var invStatus = scriptContext.newRecord.getValue('status');
+	/**
+	 * @param {search} search
+	 * @param {serverWidget} serverWidget
+	 */
+	function(search, serverWidget, runtime, itpm) {
 
-    			//invoice dont have any ITPM DEDUCTION records which is not Open,Pending
-    			var invoiceDeductionsAreEmpty = search.create({
-    				type:'customtransaction_itpm_deduction',
-    				columns:['internalid'],
-    				filters:[['custbody_itpm_ddn_invoice','is',scriptContext.newRecord.id],'and',
-    					['status','anyof',["Custom100:A","Custom100:B"]]]
-    			}).run().getRange(0,5).length == 0;
-    			
-    			if(invStatus != 'Paid In Full' && invoiceDeductionsAreEmpty){
-    				scriptContext.form.clientScriptModulePath = './iTPM_Attach_Invoice_ClientMethods.js';
-    				scriptContext.form.addButton({
-    					id:'custpage_itpm_newddn',
-    					label:'Deduction',
-    					functionName:'iTPMDeduction('+scriptContext.newRecord.id+')'
-    				});
-    			}
-    		}
-    	}catch(e){
-    		log.error(e.name,' record type = invoice, record id='+scriptContext.newRecord.id+' message='+e.message);
-    	}
-    }
+	/**
+	 * Function definition to be triggered before record is loaded.
+	 *
+	 * @param {Object} scriptContext
+	 * @param {Record} scriptContext.newRecord - New record
+	 * @param {string} scriptContext.type - Trigger type
+	 * @param {Form} scriptContext.form - Current form
+	 * @Since 2015.2
+	 */
+	function beforeLoad(scriptContext) {
+		try{
+			if(runtime.executionContext == runtime.ContextType.USER_INTERFACE && scriptContext.type == 'view'){
+				//invoice status not equal to PAID IN FULL
+				var invStatus = scriptContext.newRecord.getValue('status');
 
-    return {
-        beforeLoad: beforeLoad
-    };
-    
+				//invoice dont have any ITPM DEDUCTION records which is not Open,Pending
+				var invoiceDeductionsAreEmpty = search.create({
+					type:'customtransaction_itpm_deduction',
+					columns:['internalid'],
+					filters:[['custbody_itpm_ddn_invoice','is',scriptContext.newRecord.id],'and',
+						['status','anyof',["Custom100:A","Custom100:B"]]]
+				}).run().getRange(0,5).length == 0;
+
+				var ddnPermission = itpm.getUserPermission(runtime.getCurrentScript().getParameter('custscript_itpm_inv_ddn_permsn_rectypeid'));
+				log.debug('ddnPermission',ddnPermission);
+
+				if(invStatus == 'Open' && invoiceDeductionsAreEmpty){
+					scriptContext.form.clientScriptModulePath = './iTPM_Attach_Invoice_ClientMethods.js';
+					if(ddnPermission >= 2){
+						scriptContext.form.addButton({
+							id:'custpage_itpm_newddn',
+							label:'Deduction',
+							functionName:'iTPMDeduction('+scriptContext.newRecord.id+')'
+						});
+					}
+				}
+			}
+		}catch(e){
+			log.error(e.name,' record type = invoice, record id='+scriptContext.newRecord.id+' message='+e.message);
+		}
+	}
+
+	return {
+		beforeLoad: beforeLoad
+	};
+
 });
