3198e3741cd76fb2557e504228d9bc63e9b64c09
Ravishankar Bandaru <ravishankar@amzurtech.com>
2018-05-30 16:28:36 +0530

    Ravi : added one script for this story script name:iTPM_CS_DeductionSplit_ClientMethods.js

---
+++ iTPM_CS_DeductionSplit_ClientMethods.js
@@ -0,0 +1,117 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope TargetAccount
+ */
+define(['N/record',
+		'N/ui/message'],
+		
+function(record, message) {
+
+	/**
+	 * Function to be executed after page is initialized.
+	 *
+	 * @param {Object} scriptContext
+	 * @param {Record} scriptContext.currentRecord - Current form record
+	 * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+	 *
+	 * @since 2015.2
+	 */
+	function pageInit(scriptContext) {
+	}
+	
+	
+	/**
+	 * Validation function to be executed when sublist line is committed.
+	 *
+	 * @param {Object} scriptContext
+	 * @param {Record} scriptContext.currentRecord - Current form record
+	 * @param {string} scriptContext.sublistId - Sublist name
+	 *
+	 * @returns {boolean} Return true if sublist line is valid
+	 *
+	 * @since 2015.2
+	 */
+	function lineInit(scriptContext) {
+		var ddnSplitRec = scriptContext.currentRecord;
+		var totalAmount = 0;
+		var lineCount = ddnSplitRec.getLineCount('recmachcustrecord_itpm_split');
+		
+		//Getting all lines amount
+		for(var i = 0;i < lineCount;i++){
+			lineAmount = ddnSplitRec.getSublistValue({
+				sublistId:'recmachcustrecord_itpm_split',
+				fieldId:'custrecord_split_amount',
+				line:i
+			});
+			totalAmount += parseFloat(lineAmount);			
+		}	
+		//getting open balance from deduction record
+		var openBalance = scriptContext.currentRecord.getValue('custrecord_itpm_split_ddnopenbal');
+		
+		if(totalAmount > openBalance){
+			 var maxMsg = message.create({
+		            title: "Warning!", 
+		            message:'Total Line amount ='+totalAmount+'\nyou have entered '+(totalAmount -openBalance)+' greater than open Balance', 
+		            type: message.Type.WARNING
+		        });
+			    maxMsg.show();
+		        setTimeout(maxMsg.hide, 4000);			
+			return false;
+		}else if(totalAmount < openBalance){
+			var minMsg = message.create({
+	            title: "Split Amount Info", 
+	            message:'Total Line amount ='+totalAmount+'\nyou have Reamining '+(openBalance - totalAmount)+' amount to split',
+	            type: message.Type.INFORMATION
+	        });
+			minMsg.show();
+	        setTimeout(minMsg.hide, 3000);			
+			return false;
+		}				
+		return true;
+	}
+
+
+	/**
+	 * Validation function to be executed when record is saved.
+	 *
+	 * @param {Object} scriptContext
+	 * @param {Record} scriptContext.currentRecord - Current form record
+	 * @returns {boolean} Return true if record is valid
+	 *
+	 * @since 2015.2
+	 */
+	function saveRecord(scriptContext) {
+		var ddnSplitRec = scriptContext.currentRecord;
+		var totalAmount = 0;				
+		console.log(ddnSplitRec.getValue('custrecord_itpm_split_ddnopenbal'));
+		var lineCount = ddnSplitRec.getLineCount('recmachcustrecord_itpm_split');
+		console.log(lineCount);
+		for(var i = 0;i < lineCount;i++){
+			lineAmount = ddnSplitRec.getSublistValue({
+				sublistId:'recmachcustrecord_itpm_split',
+				fieldId:'custrecord_split_amount',
+				line:i
+			});
+			totalAmount += parseFloat(lineAmount);			
+		}
+		console.log(totalAmount);
+		var openBalance = scriptContext.currentRecord.getValue('custrecord_itpm_split_ddnopenbal');
+		if(totalAmount > openBalance){			
+			alert('Total Line amount ='+totalAmount+'\nyou have entered '+(totalAmount -openBalance)+' greater than open Balance');
+			return false;
+		}else if(totalAmount < openBalance){			
+			alert('Total Line amount ='+totalAmount+'\nyou have entered '+(openBalance- totalAmount)+' less than open Balance');
+			return false;
+		}
+		console.log(scriptContext.currentRecord.getValue('custrecord_itpm_split_ddnopenbal'));		
+		return true;
+	}
+
+	return {
+//		pageInit: pageInit,
+		lineInit: lineInit,
+		saveRecord:saveRecord
+	};
+
+});
