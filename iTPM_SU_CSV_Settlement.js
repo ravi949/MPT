8993f5ee7a23591dbf1839861746512325572262
prasad-adari <prasad.adari@amzurtech.com>
2018-06-05 20:36:41 +0530

    prasad: updated with valdations

--- iTPM_SU_CSV_Settlement.js
+++ iTPM_SU_CSV_Settlement.js
@@ -21,35 +21,63 @@ function(ui) {
     		var response = context.response;
     		
     		if(request.method == 'GET'){
-    			var form = ui.createForm({
-    				title: 'CSV Settlement Form'
-    			});
-    			
-    			form.addField({
-    	    	    id : 'custom_itpm_csvupload',
-    	    	    type : serverWidget.FieldType.FILE,
-    	    	    label : 'Upload CSV File'
-    	    	}).isMandatory = true;
-    			
-    			form.addSubmitButton({
-    				label: 'Submit'
-    			});
-    			
-    			form.addButton({
-    	    		id : 'custom_itpm_tcacel',
-    	    	    label : 'Cancel',
-    	    	    functionName:"redirectToBack"
-    	    	});
-    	    	
-    	    	form.clientScriptModulePath =  './iTPM_Bulk_Settlements_ClientMethods.js';
-    			
-    			response.writePage();
+    			csvFieUploadForm(request, response);
+    		}else{
+    			validateCSVFile(request, response);
     		}
     	}catch(e){
     		log.error(e.name, e.message);
     	}
     }
 
+    /**
+     * @param {Object} request
+     * @param {Object} response
+     */
+    function csvFieUploadForm(request, response){
+    	var form = ui.createForm({
+			title: 'CSV Settlement'
+		});
+		
+		form.addField({
+    	    id : 'custom_itpm_csvfile',
+    	    type : ui.FieldType.FILE,
+    	    label : 'Upload CSV File'
+    	}).isMandatory = true;
+		
+		form.addSubmitButton({
+			label: 'Submit'
+		});
+		
+		form.addButton({
+    		id : 'custom_itpm_tcacel',
+    	    label : 'Cancel',
+    	    functionName:"redirectToBack"
+    	});
+    	
+    	//form.clientScriptModulePath =  './iTPM_Bulk_Settlements_ClientMethods.js';
+		
+		response.writePage(form);
+    }
+    
+    /**
+     * @param {Object} request
+     * @param {Object} response
+     */
+    function validateCSVFile(request, response){
+    	var file = request.files.custom_itpm_csvfile;
+    	
+    	//throw user error for invalid file error other than .csv
+    	if(file.name.split('.')[1] == 'csv'){
+    		log.debug('file', file.name.split('.')[1]);
+    	}else{
+    		throw{
+    			name:'INVALID_FILE',
+    			message:'Please upload valid CSV file.'
+    		}
+    	}
+    }
+    
     return {
         onRequest: onRequest
     };
