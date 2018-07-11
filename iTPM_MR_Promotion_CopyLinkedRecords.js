4584add6866d1e864c13b93a1f726715fb83840d
tajrazak <sayyad.tajuddin@amzurtech.com>
2018-07-11 15:54:30 +0530

    taj:- removed the multiple join search added individual searches

--- iTPM_MR_Promotion_CopyLinkedRecords.js
+++ iTPM_MR_Promotion_CopyLinkedRecords.js
@@ -60,113 +60,55 @@ function(record, search) {
 			arrResult = searchResult.values,
 			promoID = arrResult.internalid.value,
 			copyPromoId = arrResult['internalid.CUSTRECORD_ITPM_P_COPIEDFROM'].value,
-			contextObj = null,executeResultSet = []
-
-//			log.debug('searchResult',searchResult);
-//			log.debug('promotionID',promoID);
-//			log.debug('copyPromoId',copyPromoId);
+			contextObj = null,executeResultSet = [];
 			
-			var loadedSearch = search.create({
-				   type: "customrecord_itpm_promotiondeal",
-				   filters: [
-					   ["internalid",'is',copyPromoId],"AND",
-					   ["CUSTRECORD_ITPM_PP_PROMOTION.isinactive","is",false],"AND",
-					   ["CUSTRECORD_ITPM_ALL_PROMOTIONDEAL.isinactive","is",false],"AND",
-					   ["CUSTRECORD_ITPM_REI_PROMOTIONDEAL.isinactive","is",false],"AND",
-					   ["CUSTRECORD_ITPM_ESTQTY_PROMODEAL.isinactive","is",false]
-				   ],
-				   columns: [
-				      search.createColumn({
-				         name: "internalid"
-				      }),
-				      search.createColumn({
-				    	  name: "internalid",
-				    	  join: "CUSTRECORD_ITPM_PP_PROMOTION"
-				      }),
-				      search.createColumn({
-				         name: "internalid",
-				         join: "CUSTRECORD_ITPM_P_COPIEDFROM"
-				      }),
-				      search.createColumn({
-				         name: "internalid",
-				         join: "CUSTRECORD_ITPM_ALL_PROMOTIONDEAL"
-				      }),
-				      search.createColumn({
-					     name: "internalid",
-					     join: "CUSTRECORD_ITPM_ESTQTY_PROMODEAL"
-					  }),
-				      search.createColumn({
-				         name: "internalid",
-				         join: "CUSTRECORD_ITPM_REI_PROMOTIONDEAL"
-				      })
-				   ]
-				}).run();
-			//Run the search to get all linked records without any break
-			var result,resultLength,
-        	count = 0, pageSize = 1000,
-        	currentIndex = 0, arrayofRecs =[];
-				
-				do{
-					result = loadedSearch.getRange(currentIndex, currentIndex + pageSize);
-					arrayofRecs = arrayofRecs.concat(result);
-					count = result.length;
-					currentIndex += pageSize;
-				}while(count == pageSize);
-//				log.debug('arrayofRecs',copyPromoId)
+			getSearchResults("CUSTRECORD_ITPM_PP_PROMOTION",copyPromoId).run().each(function(e){
+				contextObj = {promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_PP_PROMOTION',name:'internalid'}),type:'planning'};
+				executeResultSet.push({id:contextObj.recId,type:contextObj.type});
+				return true;
+			});
+			
+			getSearchResults("CUSTRECORD_ITPM_ALL_PROMOTIONDEAL",copyPromoId).run().each(function(e){
+				contextObj = {promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_ALL_PROMOTIONDEAL',name:'internalid'}),type:'all'};
+				executeResultSet.push({id:contextObj.recId,type:contextObj.type});
+				return true;
+			});
+			
+			getSearchResults("CUSTRECORD_ITPM_ESTQTY_PROMODEAL",copyPromoId).run().each(function(e){
+				contextObj = {type:'estqty',promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_ESTQTY_PROMODEAL',name:'internalid'})}
+				executeResultSet.push({id:contextObj.recId,type:contextObj.type});
+				return true;
+			});
+			getSearchResults("CUSTRECORD_ITPM_REI_PROMOTIONDEAL",copyPromoId).run().each(function(e){
+				contextObj = {type:'retail',promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_REI_PROMOTIONDEAL',name:'internalid'})}
+				executeResultSet.push({id:contextObj.recId,type:contextObj.type});
+				return true;
+			});
 
-
-				//Deducting the duplicates, and removing them and pushing those results into array
-				arrayofRecs.forEach(function(e){
-					if(e.getValue({join:'CUSTRECORD_ITPM_ESTQTY_PROMODEAL',name:'internalid'}) != ""){
-						contextObj = {type:'estqty',promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_ESTQTY_PROMODEAL',name:'internalid'})}
-						if(!executeResultSet.some(function(k){return (contextObj.recId == k.id && contextObj.type == k.type)})){	
-							executeResultSet.push({id:contextObj.recId,type:contextObj.type});
-						}
-					}
-					if(e.getValue({join:'CUSTRECORD_ITPM_REI_PROMOTIONDEAL',name:'internalid'}) != ""){
-						contextObj = {type:'retail',promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_REI_PROMOTIONDEAL',name:'internalid'})}
-						if(!executeResultSet.some(function(k){return (contextObj.recId == k.id && contextObj.type == k.type)})){	
-							executeResultSet.push({id:contextObj.recId,type:contextObj.type});
-						}
-					}
-					if(e.getValue({join:'CUSTRECORD_ITPM_ALL_PROMOTIONDEAL',name:'internalid'}) != ""){
-						contextObj = {promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_ALL_PROMOTIONDEAL',name:'internalid'}),type:'all'};
-						if(!executeResultSet.some(function(k){return (contextObj.recId == k.id && contextObj.type == k.type)})){	
-							executeResultSet.push({id:contextObj.recId,type:contextObj.type});
-						}
-					}
-					if(e.getValue({join:'CUSTRECORD_ITPM_PP_PROMOTION',name:'internalid'}) != ""){
-						contextObj = {promoID:promoID,copyPromoId:copyPromoId,recId:e.getValue({join:'CUSTRECORD_ITPM_PP_PROMOTION',name:'internalid'}),type:'planning'};
-						if(!executeResultSet.some(function(k){return (contextObj.recId == k.id && contextObj.type == k.type)})){	
-							executeResultSet.push({id:contextObj.recId,type:contextObj.type});
-						}
-					}
-				})
-
-				//Write the data into the context.
-				var resultLength = executeResultSet.length-1;
-				executeResultSet.forEach(function(e,index){
-					if(e.type == 'all'){
-						context.write({promoID:promoID,copyPromoId:copyPromoId,recId:e.id,type:e.type,lastResult:resultLength == index})
-					}else{
-						context.write({type:e.type,promoID:promoID,copyPromoId:copyPromoId,recId:e.id,lastResult:resultLength == index});
-					}
-				})
-
-				if(executeResultSet.length <= 0){
-					record.submitFields({
-						type: 'customrecord_itpm_promotiondeal',
-						id: promoID,
-						values: {
-							custrecord_itpm_p_copy: false,
-							custrecord_itpm_p_copyinprogress:false
-						},
-						options: {
-							enableSourcing: false,
-							ignoreMandatoryFields : true
-						}
-					});
+			//Write the data into the context.
+			var resultLength = executeResultSet.length-1;
+			executeResultSet.forEach(function(e,index){
+				if(e.type == 'all'){
+					context.write({promoID:promoID,copyPromoId:copyPromoId,recId:e.id,type:e.type,lastResult:resultLength == index})
+				}else{
+					context.write({type:e.type,promoID:promoID,copyPromoId:copyPromoId,recId:e.id,lastResult:resultLength == index});
 				}
+			})
+
+			if(executeResultSet.length <= 0){
+				record.submitFields({
+					type: 'customrecord_itpm_promotiondeal',
+					id: promoID,
+					values: {
+						custrecord_itpm_p_copy: false,
+						custrecord_itpm_p_copyinprogress:false
+					},
+					options: {
+						enableSourcing: false,
+						ignoreMandatoryFields : true
+					}
+				});
+			}
 		
 		}catch(e){
 			log.error(e.name,'Map state, message = '+e.message);
@@ -346,6 +288,34 @@ function(record, search) {
 			columns:['custitem_itpm_available']
 		}).custitem_itpm_available
 	}
+	
+	/**
+	 * @param {string} lineType
+	 * @param {number} copyPromoId
+	 */
+	function getSearchResults(lineType, copyPromoId){
+		return search.create({
+			   type: "customrecord_itpm_promotiondeal",
+			   filters: [
+				   ["internalid",'is',copyPromoId],"AND",
+				   [(lineType+".isinactive").toString(),"is",false]
+			   ],
+			   columns: [
+			      search.createColumn({
+			         name: "internalid"
+			      }),
+			      search.createColumn({
+			    	  name: "internalid",
+			    	  join: lineType
+			      }),
+			      search.createColumn({
+			         name: "internalid",
+			         join: "CUSTRECORD_ITPM_P_COPIEDFROM"
+			      })
+			   ]
+			});
+	}
+	
 	return {
 		getInputData: getInputData,
 		map: map,
