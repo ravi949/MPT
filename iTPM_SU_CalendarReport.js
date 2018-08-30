d3662cab57d9f2b89f10872140d17f757ad7f146
tajrazak <sayyad.tajuddin@amzurtech.com>
2018-08-30 17:00:50 +0530

    taj:- removed the un-neccessary condition

--- iTPM_SU_CalendarReport.js
+++ iTPM_SU_CalendarReport.js
@@ -273,10 +273,6 @@ function(render, search, runtime, file, record, util, serverWidget, itpm) {
 				promoData.forEach(function(promo){
 					fileOutput += "\n\""+promo.entity+"\","+promo.item+",\""+promo.promo_type+"\",\""+promo.promo_desc+"\",\""+promo.promo_id+"\","+promo.ship_startdate+","+promo.ship_enddate+","+promo.uom+","+promo.mop+","+promo.percent_peruom+","+promo.rate_peruom+",";
 					sundaysList.forEach(function(e){
-						//this condition for to add the statuses in for promotion which are active in between the calendar start and end dates
-						if(parseInt(e.year) < parseInt(promo.eyear)){
-							promo.eyear = e.year;
-						}
 						if(e.startMonth <= e.month && e.month <= e.endMonth){
 							if(promo.syear == promo.eyear){
 								if(promo.syear == e.year && promo.sweek <= e.week && e.week <= promo.eweek){
@@ -485,6 +481,7 @@ function(render, search, runtime, file, record, util, serverWidget, itpm) {
     					status = {text:'Closed',cls:'status-blue'};
     				}
     				
+    				//this condition for to add the statuses in for promotion which are active in between the calendar start and end dates
     				var isPromoEndDateYearGreaterThanCalEndDate = parseInt(new Date(result.getValue({ name: 'custrecord_itpm_p_shipend' })).getFullYear()) > parseInt(endDateYear);
     				var isPromoStartDateYearLessThanCalStartDate = parseInt(new Date(result.getValue({ name: 'custrecord_itpm_p_shipstart' }))) < parseInt(startDateYear);
     				
@@ -508,7 +505,7 @@ function(render, search, runtime, file, record, util, serverWidget, itpm) {
     	    		    smonth 		  : (isPromoStartDateYearLessThanCalStartDate)? startMonth : new Date(result.getValue({ name: 'custrecord_itpm_p_shipstart' })).getMonth(),
     	    		    emonth 		  : (isPromoEndDateYearGreaterThanCalEndDate)? endMonth : new Date(result.getValue({ name: 'custrecord_itpm_p_shipend' })).getMonth(),
     	    		    syear		  : new Date(result.getValue({ name: 'custrecord_itpm_p_shipstart' })).getFullYear(),
-    	    		    eyear		  : new Date(result.getValue({ name: 'custrecord_itpm_p_shipend' })).getFullYear()
+    	    		    eyear		  : (isPromoEndDateYearGreaterThanCalEndDate)? endDateYear : new Date(result.getValue({ name: 'custrecord_itpm_p_shipend' })).getFullYear()
     		        });
     			   return true;
     			});
