da23d9f8bc2bf16b80bd5981baa5916f6522f683
Ravishankar Bandaru <ravishankar@amzurtech.com>
2018-09-12 21:27:09 +0530

    Added banner if accounting period is closed and created one suitelet script for getting status

--- iTPM_SU_GetAccountingPeriod_Status.js
+++ iTPM_SU_GetAccountingPeriod_Status.js
@@ -18,24 +18,28 @@ define(['N/search'],
 	 * @Since 2015.2
 	 */
 	function onRequest(context) {
-		if(context.request.method == 'GET'){
-			log.debug('params',context.request.parameters);
+		try{
+			if(context.request.method == 'GET'){
+				log.debug('params',context.request.parameters);
 
-			//search for account period is closed or not
-			var postingPeriodId = context.request.parameters.popid;
-			var accountingperiodSearchObj = search.create({
-				type: "accountingperiod",
-				filters:
-					[
-						["alllocked","is","T"], 
-						"AND", 
-						["internalid","anyof",postingPeriodId]
-						],
-						columns:['internalid']
-			});
-			var isAcntngprdClosed = accountingperiodSearchObj.runPaged().count;
-			log.debug("accountingperiodSearchObj result count",isAcntngprdClosed);
-			context.response.write(JSON.stringify({success : true, period_closed : isAcntngprdClosed > 0 }));
+				//search for account period is closed or not
+				var postingPeriodId = context.request.parameters.popid;
+				var accountingperiodSearchObj = search.create({
+					type: "accountingperiod",
+					filters:
+						[
+							["alllocked","is","T"], 
+							"AND", 
+							["internalid","anyof",postingPeriodId]
+							],
+							columns:['internalid']
+				});
+				var isAcntngprdClosed = accountingperiodSearchObj.runPaged().count;
+				log.debug("accountingperiodSearchObj result count",isAcntngprdClosed);
+				context.response.write(JSON.stringify({success : true, period_closed : isAcntngprdClosed > 0 }));
+			}
+		}catch(ex){
+			log.error(ex.name,ex.message);
 		}
 
 	}
